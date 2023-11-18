1f73cbc480b8a2edc231858425bb8b3360c9c383
hassakhalil <hkhalil@student.1337.ma>
2023-11-18 13:51:33 +0100

    added Makefile to Encapsulation module

--- Encapsulation/DivideAndRule.hpp
+++ Encapsulation/DivideAndRule.hpp
@@ -1,3 +1,6 @@
+#ifndef DIVIDEANDRULE_HPP
+#define DIVIDEANDRULE_HPP
+
 #include <iostream>
 #include <vector>
 #include <map>
@@ -129,42 +132,4 @@ struct Bank
 
 };
 
-int main()
-{
-    try{
-        // Account accountA = Account();
-        // accountA.id = 0;
-        // accountA.value = 100;
-
-        // Account accountB = Account();
-        // accountB.id = 1;
-        // accountB.value = 100;
-
-        Bank bank = Bank();
-        bank.addLiquidty(1000);
-        Bank::Account *clientAccount = bank.createAccount();
-        bank.addMoneytoAccount(clientAccount->getId(), 100);
-        bank.giveLoan(clientAccount->getId(), 50);
-        
-        // bank.liquidity = 999;
-        // bank.clientAccounts.push_back(&accountA);
-        // bank.clientAccounts.push_back(&accountB);
-
-        // bank.liquidity -= 200;
-        // accountA.value += 400;
-
-        // std::cout << "Account : " << std::endl;
-        // // std::cout << accountA << std::endl;
-        // // std::cout << accountB << std::endl;
-
-        // std::cout << " ----- " << std::endl;
-
-        std::cout << "Bank : " << std::endl;
-        std::cout << bank << std::endl;
-    }
-    catch(const std::exception& e){
-        std::cerr << e.what() << '\n';
-    }
-
-        return (0);
-}
+#endif
